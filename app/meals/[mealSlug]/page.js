7fc8bd96f479442d409880db5e0446c006963e2c
moti.vatandoust <moti.vatandoust@gmail.com>
2025-01-30 17:03:29 +0330

    add meal details page with dynamic content

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,31 @@
-export default function MealDetailsPage() {
-    return (
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import classes from "./page.module.css";
+export default function MealDetailsPage({params}) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.ceator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
       <main>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
-          Meal Details
-        </h1>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions
+          }}
+        ></p>
       </main>
-    );
-  }
+    </>
+  );
+}
