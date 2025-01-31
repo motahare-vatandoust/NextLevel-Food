e92af14f3bbf8dae032474bd10a7ea7d210a6611
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-01 00:37:26 +0330

    Add input validation and error handling for meal sharing functionality

---
+++ app/meals/share/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error accured!</h1>
+      <p>Failed to create meal. Please try again later. </p>
+    </main>
+  );
+}
